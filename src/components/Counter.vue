16b6eccf3a1d936680e2de5d7fe4147a61cd4e08
Juanma <juanmaper30@gmail.com>
2022-05-30 17:06:52 +0200

    Add state properties and use them as computed

---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Counter - Vuex</h1>
+  <h2>Direct access: {{ $store.state.count }}</h2>
+  <h2>Computed: {{ countComputed }}</h2>
+
+  <h1>mapState</h1>
+  <h2>mapState: {{ count }}</h2>
+  <h2>lastMutation: {{ lastMutation }}</h2>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+
+  computed: {
+    countComputed() {
+      return this.$store.state.count
+    },
+    ...mapState(['count', 'lastMutation'])
+  }
+}
+</script>
