ca52fef4851d476fd22054d7ebc6ebea7fdac463
Juanma <juanmaper30@gmail.com>
2022-05-31 14:08:21 +0200

    Implement logic using modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,44 +1,9 @@
-import getRandomInt from '@/helpers/getRandomInt'
 import { createStore } from 'vuex'
+import counterStore from './counter'
 
 export default createStore({
 
-  state: {
-    count: 1,
-    lastMutation: 'none',
-    isLoading: false,
-    lastRandomInt: 0
-  },
-
-  mutations: {
-    increment( state ) {
-      state.count++
-      state.lastMutation = 'increment'
-    },
-    incrementBy( state, value ) {
-      state.count += value
-      state.lastMutation = 'incrementBy ' + value
-      state.lastRandomInt = value
-    },
-    setLoading( state, value ) {
-      state.isLoading = value
-    }
-  },
-
-  actions: {
-    async incrementRandomInt({ commit }) {
-
-      commit('setLoading', true)
-      const randomInt = await getRandomInt()
-
-      commit('incrementBy', randomInt)
-      commit('setLoading', false)
-    }
-  },
-
-  getters: {
-    squaredCount( state ) {
-      return state.count ** 2
-    }
+  modules: {
+    counter: counterStore
   }
 }) 
